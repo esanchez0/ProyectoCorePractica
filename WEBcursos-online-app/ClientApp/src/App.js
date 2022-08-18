0ffd2f3e43da391e0241e443d3353800467cad50
EDER SANCHEZ <EDER_UNAM_DF@hotmail.com>
2022-08-17 20:02:15 -0500

    Iniciando react

--- WEBcursos-online-app/ClientApp/src/App.js
+++ WEBcursos-online-app/ClientApp/src/App.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
-import './custom.css'
+//import React, { Component } from 'react';
+import React, { useState } from 'react'
+//import './custom.css'
+import Perfil from './components/Perfil'
 
-export default class App extends Component {
-    static displayName = App.name;
+//export default class App extends Component {
+//    static displayName = App.name;
 
-    render() {
-        return (
-            <div>
-                <h1>
-                    Bienvenidos al curso de asp net y react hook
-                </h1>
-            </div>
-        );
+//    render() {
+//        const [nombre, cambiarNombre] = useState('No tiene nombre');
+
+//        return (
+//            <div>
+//                <h1>
+//                    Bienvenidos al curso de asp net y react hook
+//                </h1>
+//                <input name="nombre" type="text" />
+//            </div>
+//        );
+//    }
+//}
+
+function App() {
+    var [nombre, cambiarNombre] = useState('No tiene nombre'); //Nombre de la variable, nombre del metodo con el que va cambiar, valor inicializado
+
+    function eventoCajaTexto(e) {
+        cambiarNombre(e.target.value)
     }
+
+    return (
+        <div>
+            <h1>
+                Bienvenidos al curso de asp net y react hook {nombre}
+            </h1>
+            <input name="nombre" type="text" value={nombre} onChange={eventoCajaTexto} />
+
+            <Perfil/>
+
+        </div>
+    );
+
 }
+
+export default App;
---
+++ WEBcursos-online-app/ClientApp/src/components/Perfil.js
@@ -0,0 +1,45 @@
+﻿import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
+
+function Perfil(props) {
+
+    const [paises, obtenerPaises] = useState([]);
+    const [status, cambiarStatus] = useState(false);
+
+    useEffect(() => {
+
+        axios.get('https://restcountries.com/v2/all')
+            .then(resultado => {
+                obtenerPaises(resultado.data);
+                cambiarStatus(true);
+            })
+            .catch(error => {
+                cambiarStatus(true);
+            })
+
+    }, [])
+
+
+    if (status) {
+        return (
+
+            <ul>
+                {paises.map((pais, indice) =>
+                    <li key={indice}>
+                        {pais.name}
+                    </li>
+
+                )}
+            </ul>
+
+        );
+    }
+    else {
+        return (<h1> Esta cargando los valores de la rest service....</h1>)
+    }
+
+
+}
+
+export default Perfil;
